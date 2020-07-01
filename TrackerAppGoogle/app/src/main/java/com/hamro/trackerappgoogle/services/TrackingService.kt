dcee6216824e77fb7ad4586190735ccf9f6a069b
Santosh <get.santoshbhandari@gmail.com>
2020-07-01 22:17:37 +0545

    foreground-services

--- TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/services/TrackingService.kt
+++ TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/services/TrackingService.kt
@@ -1,20 +1,38 @@
 package com.hamro.trackerappgoogle.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.hamro.trackerappgoogle.R
 import com.hamro.trackerappgoogle.others.Constants.ACTION_PAUSE_SERVICE
+import com.hamro.trackerappgoogle.others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import com.hamro.trackerappgoogle.others.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.hamro.trackerappgoogle.others.Constants.ACTION_STOP_SERVICE
+import com.hamro.trackerappgoogle.others.Constants.NOTIFICATION_CHANNEL_ID
+import com.hamro.trackerappgoogle.others.Constants.NOTIFICATION_CHANNEL_NAME
+import com.hamro.trackerappgoogle.others.Constants.NOTIFICATION_ID
+import com.hamro.trackerappgoogle.ui.MainActivity
 
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
-
+    var isFirstRun = true
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
             when(it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
-                    Timber.d("Started or resumed service")
+                    if(isFirstRun) {
+                        startForegroundService()
+                        isFirstRun = false
+                    } else {
+                        Timber.d("Resuming service...")
+                    }
                 }
                 ACTION_PAUSE_SERVICE -> {
                     Timber.d("Paused service")
@@ -25,5 +43,43 @@ class TrackingService : LifecycleService() {
             }
         }
         return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForegroundService() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE)
+                as NotificationManager
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(NOTIFICATION_ID, notificationBuilder.build())
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        PendingIntent.FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            NOTIFICATION_CHANNEL_ID,
+            NOTIFICATION_CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
     }
 }
--- TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/MainActivity.kt
+++ TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.hamro.trackerappgoogle.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
@@ -8,6 +9,7 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.hamro.trackerappgoogle.R
 import com.hamro.trackerappgoogle.db.RunDAO
+import com.hamro.trackerappgoogle.others.Constants.ACTION_SHOW_TRACKING_FRAGMENT
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
@@ -18,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigateToTrackingFragmentIfNeeded(intent)
 
         setSupportActionBar(toolbar)
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
@@ -30,5 +33,15 @@ class MainActivity : AppCompatActivity() {
                     else -> bottomNavigationView.visibility = View.GONE
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+    private fun navigateToTrackingFragmentIfNeeded(intent: Intent?) {
+        if(intent?.action == ACTION_SHOW_TRACKING_FRAGMENT) {
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 }
