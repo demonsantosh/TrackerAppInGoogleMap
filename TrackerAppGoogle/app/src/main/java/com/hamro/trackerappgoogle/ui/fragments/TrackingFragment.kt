a90c562aca285d6754a5645fce70c4a6a4b9200e
Santosh <get.santoshbhandary@gmail.com>
2020-07-28 10:55:41 +0545

    save and run to db

--- TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/fragments/TrackingFragment.kt
+++ TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/fragments/TrackingFragment.kt
@@ -10,9 +10,12 @@ import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.hamro.trackerappgoogle.R
+import com.hamro.trackerappgoogle.db.Run
 import com.hamro.trackerappgoogle.others.Constants.ACTION_PAUSE_SERVICE
 import com.hamro.trackerappgoogle.others.Constants.ACTION_START_OR_RESUME_SERVICE
 import com.hamro.trackerappgoogle.others.Constants.ACTION_STOP_SERVICE
@@ -25,6 +28,8 @@ import com.hamro.trackerappgoogle.services.TrackingService
 import com.hamro.trackerappgoogle.ui.viewmodels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_tracking.*
+import java.util.*
+import kotlin.math.round
 
 @AndroidEntryPoint
 class TrackingFragment : Fragment(R.layout.fragment_tracking) {
@@ -36,6 +41,8 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
     private var map: GoogleMap? = null
     private var curTimeInMillis = 0L
     private var menu: Menu? = null
+
+    private var weight = 80f
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -52,9 +59,16 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         btnToggleRun.setOnClickListener {
             toggleRun()
         }
+
+        btnFinishRun.setOnClickListener {
+            zoomToSeeWholeTrack()
+            endRunAndSaveToDb()
+        }
         mapView.getMapAsync {
             map = it
+            addAllPolylines()
         }
+
         subscribeToObservers()
     }
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -145,6 +159,42 @@ class TrackingFragment : Fragment(R.layout.fragment_tracking) {
         }
     }
 
+    private fun zoomToSeeWholeTrack() {
+        val bounds = LatLngBounds.Builder()
+        for(polyline in pathPoints) {
+            for(pos in polyline) {
+                bounds.include(pos)
+            }
+        }
+
+        map?.moveCamera(
+            CameraUpdateFactory.newLatLngBounds(
+                bounds.build(),
+                mapView.width,
+                mapView.height,
+                (mapView.height * 0.05f).toInt()
+            )
+        )
+    }
+    private fun endRunAndSaveToDb() {
+        map?.snapshot { bmp ->
+            var distanceInMeters = 0
+            for(polyline in pathPoints) {
+                distanceInMeters += TrackingUtility.calculatePolylineLength(polyline).toInt()
+            }
+            val avgSpeed = round((distanceInMeters / 1000f) / (curTimeInMillis / 1000f / 60 / 60) * 10) / 10f
+            val dateTimestamp = Calendar.getInstance().timeInMillis
+            val caloriesBurned = ((distanceInMeters / 1000f) * weight).toInt()
+            val run = Run(bmp, dateTimestamp, avgSpeed, distanceInMeters, curTimeInMillis, caloriesBurned)
+            viewModel.insertRun(run)
+            Snackbar.make(
+                requireActivity().findViewById(R.id.rootView),
+                "Run saved successfully",
+                Snackbar.LENGTH_LONG
+            ).show()
+            stopRun()
+        }
+    }
     private fun addAllPolylines() {
         for(polyline in pathPoints) {
             val polylineOptions = PolylineOptions()
--- TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/viewmodels/MainViewModel.kt
+++ TrackerAppGoogle/app/src/main/java/com/hamro/trackerappgoogle/ui/viewmodels/MainViewModel.kt
@@ -2,9 +2,17 @@ package com.hamro.trackerappgoogle.ui.viewmodels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hamro.trackerappgoogle.db.Run
 import com.hamro.trackerappgoogle.repositories.MainRepository
+import kotlinx.coroutines.launch
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
